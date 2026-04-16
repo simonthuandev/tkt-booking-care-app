a9da7a8ff92529a1283e29ef6db36b104bc709c1
2k3hocviec <khoahoian0322003@gmail.com>
2026-04-16 17:10:57 +0700

    Tạo giao diện phần ForgotPasswordPage

--- src/pages/AuthPages/ForgotPasswordPage.jsx
+++ src/pages/AuthPages/ForgotPasswordPage.jsx
@@ -1,11 +1,49 @@
+import { Link } from "react-router";
+import { BrandLogo } from "../../components/Common/BrandLogo";
 import RoutePage from "../../components/Common/RoutePage";
 
 const ForgotPasswordPage = () => {
   return (
-    <RoutePage
-      title="Forgot Password"
-      description="Request a password reset link."
-    />
+    <div className="login-container min-vh-100 d-flex flex-column align-items-center justify-content-center p-4">
+      {/* Logo */}
+      <div className="p-3">
+        <BrandLogo />
+      </div>
+
+      {/* Email Card */}
+      <div
+        className="card shadow-lg p-5"
+        style={{ width: "100%", maxWidth: "420px" }}
+      >
+        <h2>Quên mật khẩu ?</h2>
+        <p>Điền email gắn với tài khoản của bạn để khôi phục</p>
+        {/* Email Input */}
+        <div className="mb-4 ">
+          <label className="form-label fw-semibold text-secondary">
+            E-mail
+          </label>
+          <input
+            type="email"
+            placeholder=""
+            className="form-control form-control-lg"
+          />
+        </div>
+
+        {/* Button go to gmail */}
+        <button className="btn btn-primary btn-lg w-100 fw-semibold mb-4">
+          Tiếp tục
+        </button>
+
+        {/* Goback Login */}
+        <div className="d-flex justify-content-center align-items-center text-secondary">
+          <Link to="/auth/login">
+            <a href="#" className="text-secondary text-decoration-none">
+              Quay lại trang đăng nhập
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
