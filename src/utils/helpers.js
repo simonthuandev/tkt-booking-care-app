2852fa3583718e954ce0d7101770b27cb79e1adc
simonthuandev-cachy <nguyenducthuank588@gmail.com>
2026-06-12 23:04:50 +0700

    refactor: remove the old_design_system plain text file and the unused helper folder

--- src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export const toSlug = (value) =>
-  value
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/đ/g, "d")
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/(^-|-$)/g, "");
